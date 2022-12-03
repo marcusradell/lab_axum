a21f643c60530a15f77d5c4f5b714746963b0eba
Marcus Rådell <marcus@radell.net>
2022-12-03 20:20:21 +0100

    add migrations from monadium

---
+++ migrations/20210527195516_identities_events.sql
@@ -0,0 +1,26 @@
+
+create schema if not exists identities;
+
+create table if not exists identities.events (
+    -- Used to order events.
+    sequence_num bigserial not null,
+    -- ID for the entity that the events apply to.
+    stream_id uuid not null,
+    -- Used to prevent concurrent updates on the same stream.
+    version int not null,
+    -- Event type, example: identity/CREATED.
+    event_type text not null,
+    -- Event payload containing all the state changes.
+    data jsonb not null,
+    -- Event metadata that helps track the events through our system.
+    cid uuid not null,
+    -- When the event got inserted into the DB.
+    inserted_at timestamptz not null default now(),
+    -- We primarily want to query the events in sequence.
+    primary key (sequence_num),
+    -- We block an update if it is based on an old stream version.
+    unique (stream_id, version)
+);
+
+-- Helps us to query for all the events for a specific stream.
+CREATE INDEX idx_event_stream_id ON identities.events (stream_id);
