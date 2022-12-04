d4cfdc64d27ea24102ebef24e065da83e1ed43ae
Marcus Rådell <marcus@radell.net>
2022-12-04 21:52:17 +0100

    remove type annotation

--- src/domains/identities/list/repo.rs
+++ src/domains/identities/list/repo.rs
@@ -2,12 +2,14 @@ use crate::domains::identities::{create::Event, repo::Repo};
 use crate::result::Result;
 use async_trait::async_trait;
 use sqlx::types::Json;
+use sqlx::FromRow;
 
 #[async_trait]
 pub trait ListRepo {
     async fn list(&self) -> Result<Vec<Event>>;
 }
 
+#[derive(FromRow)]
 struct Row {
     data: Json<Event>,
 }
@@ -15,7 +17,7 @@ struct Row {
 #[async_trait]
 impl ListRepo for Repo {
     async fn list(&self) -> Result<Vec<Event>> {
-        let rows: Vec<Row> = sqlx::query_as!(
+        let rows = sqlx::query_as!(
             Row,
             r#"select data  as "data: Json<Event>"  from identities.events"#
         )
