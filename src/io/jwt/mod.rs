994da699e706db32bc5624e4321fce419badd063
Marcus Rådell <marcus@radell.net>
2022-12-05 22:22:11 +0100

    add jwt constructor

--- src/io/jwt/mod.rs
+++ src/io/jwt/mod.rs
@@ -1,1 +1,15 @@
+use jsonwebtoken::EncodingKey;
+
 pub mod claims;
+
+pub struct Jwt {
+    encodingKey: EncodingKey,
+}
+
+impl Jwt {
+    pub fn new(secret: &str) -> Self {
+        Self {
+            encodingKey: EncodingKey::from_secret(secret.as_ref()),
+        }
+    }
+}
