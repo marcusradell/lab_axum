7a8feaa60edf35e0b5e74977402f3514aea13ed3
Marcus Rådell <marcus@radell.net>
2022-12-05 23:26:44 +0100

    add decode

--- src/io/jwt/mod.rs
+++ src/io/jwt/mod.rs
@@ -1,17 +1,19 @@
 use crate::result::Result;
 use claims::Claims;
-use jsonwebtoken::{encode, EncodingKey, Header};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
 
 pub mod claims;
 
 pub struct Jwt {
     encodingKey: EncodingKey,
+    decodingKey: DecodingKey,
 }
 
 impl Jwt {
     pub fn new(secret: &str) -> Self {
         Self {
             encodingKey: EncodingKey::from_secret(secret.as_ref()),
+            decodingKey: DecodingKey::from_secret(secret.as_ref()),
         }
     }
 
@@ -19,5 +21,11 @@ impl Jwt {
         let token = encode(&Header::default(), claims, &self.encodingKey)?;
 
         Ok(token)
+    }
+
+    pub fn decode(&self, token: &str) -> Result<Claims> {
+        let token = decode::<Claims>(&token, &self.decodingKey, &Validation::default())?;
+
+        Ok(token.claims)
     }
 }
