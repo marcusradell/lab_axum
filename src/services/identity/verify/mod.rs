6c52033ff931d3da5a2868b815c20b6ecba6bb33
Marcus Rådell <marcus@radell.net>
2023-03-07 13:54:22 +0100

    check the code against hardcoded verification code

--- src/services/identity/verify/mod.rs
+++ src/services/identity/verify/mod.rs
@@ -1,3 +1,5 @@
+use std::io::{Error, ErrorKind};
+
 use self::repo::CreateIdentityRepo;
 use super::events::CreatedEvent;
 use crate::{io::jwt::Jwt, result::Result};
@@ -16,12 +18,21 @@ pub struct Output {
     pub token: String,
 }
 
+const CORRECT_CODE: &str = "abc123";
+
 pub async fn handler(
     repo: &impl CreateIdentityRepo,
     jwt: &Jwt,
     event: CreatedEvent,
     code: String,
 ) -> Result<Output> {
+    // TODO: implement DB lookup to find the right code.
+    let correct_code = CORRECT_CODE;
+
+    if code != correct_code {
+        return Err(Box::new(Error::new(ErrorKind::InvalidInput, "Wrong code")));
+    }
+
     repo.create_identity(&event).await?;
     let token = jwt.encode(&event.stream_id, &event.role)?;
     Ok(Output { token })
