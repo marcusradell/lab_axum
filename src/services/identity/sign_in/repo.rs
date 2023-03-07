f1920c34a545ab6a5d36624e2979f5fc9aaa5edf
Marcus Rådell <marcus@radell.net>
2023-03-07 13:54:43 +0100

    move the repo fn that creates a verification

--- src/services/identity/sign_in/repo.rs
+++ src/services/identity/sign_in/repo.rs
@@ -2,6 +2,8 @@ use crate::io::repo::Repo;
 use crate::result::Result;
 use crate::services::identity::events::CreatedEvent;
 use async_trait::async_trait;
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
 
 #[async_trait]
 pub trait CreateRepo {
@@ -22,6 +24,36 @@ impl CreateRepo for Repo {
                 created_event.inserted_at.into(),
                 vec![],
             )
+            .exec()
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[async_trait]
+pub trait CreateVerificationRepo {
+    async fn create_verification(
+        &self,
+        id: Uuid,
+        email: String,
+        code: String,
+        inserted_at: DateTime<Utc>,
+    ) -> Result<()>;
+}
+
+#[async_trait]
+impl CreateVerificationRepo for Repo {
+    async fn create_verification(
+        &self,
+        id: Uuid,
+        email: String,
+        code: String,
+        inserted_at: DateTime<Utc>,
+    ) -> Result<()> {
+        self.prisma_client
+            .identity_verification()
+            .create(id.to_string(), email, code, inserted_at.into(), vec![])
             .exec()
             .await?;
 
--- src/services/identity/verify/repo.rs
+++ src/services/identity/verify/repo.rs
@@ -2,23 +2,10 @@ use crate::io::repo::Repo;
 use crate::result::Result;
 use crate::services::identity::events::CreatedEvent;
 use async_trait::async_trait;
-use chrono::{DateTime, Utc};
-use uuid::Uuid;
 
 #[async_trait]
 pub trait CreateIdentityRepo {
     async fn create_identity(&self, created_event: &CreatedEvent) -> Result<()>;
-}
-
-#[async_trait]
-pub trait CreateVerificationRepo {
-    async fn create_verification(
-        &self,
-        id: Uuid,
-        email: String,
-        code: String,
-        inserted_at: DateTime<Utc>,
-    ) -> Result<()>;
 }
 
 #[async_trait]
@@ -35,25 +22,6 @@ impl CreateIdentityRepo for Repo {
                 created_event.inserted_at.into(),
                 vec![],
             )
-            .exec()
-            .await?;
-
-        Ok(())
-    }
-}
-
-#[async_trait]
-impl CreateVerificationRepo for Repo {
-    async fn create_verification(
-        &self,
-        id: Uuid,
-        email: String,
-        code: String,
-        inserted_at: DateTime<Utc>,
-    ) -> Result<()> {
-        self.prisma_client
-            .identity_verification()
-            .create(id.to_string(), email, code, inserted_at.into(), vec![])
             .exec()
             .await?;
 
