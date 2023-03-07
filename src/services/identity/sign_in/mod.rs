5652b2a4f262e162a15c8620eacb0dd13f6a0797
Marcus Rådell <marcus@radell.net>
2023-03-07 14:17:08 +0100

    generate the verification code

--- src/services/identity/sign_in/mod.rs
+++ src/services/identity/sign_in/mod.rs
@@ -1,6 +1,7 @@
 use self::repo::CreateVerificationRepo;
 use crate::result::Result;
 use chrono::Utc;
+use rand::{distributions::Alphanumeric, Rng};
 use serde::Deserialize;
 use uuid::Uuid;
 
@@ -11,8 +12,16 @@ pub struct Input {
     pub email: String,
 }
 
+fn generate_verification_code() -> String {
+    rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(6)
+        .map(char::from)
+        .collect()
+}
+
 pub async fn handler(repo: &impl CreateVerificationRepo, email: String) -> Result<()> {
-    let code = "abc123";
+    let code = generate_verification_code();
     repo.create_verification(Uuid::new_v4(), email, code.to_string(), Utc::now())
         .await?;
 
